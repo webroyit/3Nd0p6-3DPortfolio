7ffa1ddc33af45d33e7c0a4d423b1ad47f448ab8
webroyit <xytank@gmail.com>
2023-11-22 13:00:48 -0500

    Add Higher Order Components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
     return (
@@ -55,8 +56,7 @@ const About = () => {
             ))}
         </div>
     </>
-    
   )
 }
 
-export default About;
+export default SectionWrapper(About, "about");
