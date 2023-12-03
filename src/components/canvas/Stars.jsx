04b21c34537b3b43b4bcc56294a4451b869fdb52
webroyit <xytank@gmail.com>
2023-12-03 14:42:33 -0500

    Moving Stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
@@ -8,6 +8,12 @@ const Stars = (props) => {
     const ref = useRef();
 
     const sphere = random.inSphere(new Float32Array(5000), {radius: 1.2});
+
+    // Delta means change
+    useFrame((state, delta) => {
+        ref.current.rotation.x -= delta / 10;
+        ref.current.rotation.y -= delta / 15;
+    })
 
     return (
         <group rotation={[0, 0, Math.PI / 4]}>
